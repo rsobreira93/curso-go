0c215fc6fc23630bced605149cea88e76716d27e
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-31 10:44:44 -0300

    feat: Add CreateUserDTO to dto.go

--- 9-APIS/internal/dto/dto.go
+++ 9-APIS/internal/dto/dto.go
@@ -4,3 +4,9 @@ type CreateProductDTO struct {
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
+
+type CreateUserDTO struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
