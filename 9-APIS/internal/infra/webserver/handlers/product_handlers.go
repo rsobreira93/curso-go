c225975949a7e0b2c50fba02b298fbf2a7c8c31f
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-31 09:39:06 -0300

    feat: Add GetProduct handler to retrieve a product by ID

--- 9-APIS/internal/infra/webserver/handlers/product_handlers.go
+++ 9-APIS/internal/infra/webserver/handlers/product_handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/rsobreira93/curso-go/9-APIS/internal/dto"
 	"github.com/rsobreira93/curso-go/9-APIS/internal/entity"
 	"github.com/rsobreira93/curso-go/9-APIS/internal/infra/database"
@@ -45,4 +46,24 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product, err := h.ProductDb.FindById(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
 }
