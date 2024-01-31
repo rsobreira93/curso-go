0aec619cdd0c6c22635131a7d8e29f3b01fa1dfc
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-31 09:52:23 -0300

    feat: Add UpdateProduct handler to product_handlers.go

--- 9-APIS/internal/infra/webserver/handlers/product_handlers.go
+++ 9-APIS/internal/infra/webserver/handlers/product_handlers.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rsobreira93/curso-go/9-APIS/internal/dto"
 	"github.com/rsobreira93/curso-go/9-APIS/internal/entity"
 	"github.com/rsobreira93/curso-go/9-APIS/internal/infra/database"
+	entityPkg "github.com/rsobreira93/curso-go/9-APIS/pkg/entity"
 )
 
 type ProductHandler struct {
@@ -66,4 +67,45 @@ func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var product entity.Product
+
+	err := json.NewDecoder(r.Body).Decode(&product)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product.ID, err = entityPkg.ParseID(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err = h.ProductDb.FindById(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	err = h.ProductDb.Update(&product)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
