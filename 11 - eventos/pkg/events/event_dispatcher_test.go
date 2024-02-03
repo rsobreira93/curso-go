4b9813f8b916799f762b569a2c84794bb420d7e9
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-02-03 08:30:47 -0300

    test: add waitGroup to method handler

--- 11 - eventos/pkg/events/event_dispatcher_test.go
+++ 11 - eventos/pkg/events/event_dispatcher_test.go
@@ -1,6 +1,7 @@
 package events
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -30,8 +31,7 @@ type TestEventHandler struct {
 	ID int
 }
 
-func (h *TestEventHandler) Handle(event EventInterface) {
-}
+func (h *TestEventHandler) Handle(event EventInterface, waitGroup *sync.WaitGroup) {}
 
 type EventDispatcherTestSuite struct {
 	suite.Suite
@@ -124,22 +124,28 @@ type MockHandler struct {
 	mock.Mock
 }
 
-func (m *MockHandler) Handle(event EventInterface) {
+func (m *MockHandler) Handle(event EventInterface, waitGroup *sync.WaitGroup) {
 	m.Called(event)
+	waitGroup.Done()
 }
 
 func (suite *EventDispatcherTestSuite) TestEventDispatcher_Dispatch() {
 	eh := &MockHandler{}
-
 	eh.On("Handle", &suite.event)
 
+	eh2 := &MockHandler{}
+	eh2.On("Handle", &suite.event)
+
 	suite.eventDispatcher.Register(suite.event.GetName(), eh)
+	suite.eventDispatcher.Register(suite.event.GetName(), eh2)
 
 	suite.eventDispatcher.Dispatch(&suite.event)
 
 	eh.AssertExpectations(suite.T())
+	eh2.AssertExpectations(suite.T())
 
 	eh.AssertNumberOfCalls(suite.T(), "Handle", 1)
+	eh2.AssertNumberOfCalls(suite.T(), "Handle", 1)
 
 }
 
